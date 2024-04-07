e917d35b90eb2a83bcc769dcc59c2ab8d5d83a65
Sahil Meena <sahilmeena2031@gmail.com>
2024-04-07 12:12:40 +0530

    fixed jwt

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,36 +1,48 @@
 import React, { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // @ts-ignore
 import { SignupInput } from "@sahilxdev/medium-common";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
   const [postInputs, setPostInputs] = useState<SignupInput>({
     name: "",
     username: "",
     password: ""
   });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,postInputs);
+      // const jwt = response.data;
+      // localStorage.setItem("token", jwt);
+      const jwtValue = response.data.jwt;
+      localStorage.setItem("token", jwtValue);
+      navigate('/blogs');
+    } catch(e) {
+      const errorMessage = type === "signin" ? "Signing In" : "Signing Up";
+      alert(`Error While ${errorMessage}`);
+    }
+
+  }
+
   return (
     <div className="h-screen flex justify-center flex-col items-center">
-      <div>
-
-      {
-        postInputs.password
-      }
-      </div>
-
-
       <div className="text-3xl font-extrabold">Create an account</div>
-      <div className="text-lg font-light text-slate-400">
-        Already have an account?
-        <Link className="pl-2 underline hover:text-blue-500" to={"/signin"}>
-          Login
+      <div className="mb-7 text-lg font-light text-slate-400">
+        {type === "signup" ? "Already have an account?" : "Don't have an account?"}
+        <Link className="pl-2 underline hover:text-blue-500" to={type === "signup" ? "/signin" : "/signup"}>
+        {type === "signup" ? "Sign in" : "Sign up"}
         </Link>
+        </div>
 
-        <LabelledInput label="Name" placeholder="Enter your name..." onchangefunc={(e) => {
+        {type === "signup" ? <LabelledInput label="Name" placeholder="Enter your name..." onchangefunc={(e) => {
           setPostInputs({...postInputs,
           name: e.target.value
         })
-        }} />
+        }} /> : null}
         <LabelledInput label="Username " placeholder="Enter your username..." onchangefunc={(e) => {
           setPostInputs({...postInputs,
           username: e.target.value
@@ -42,8 +54,7 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
         })
         }} />
         
-      </div>
-      <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">{type === "signup" ? "Sign Up" : "Sign In"}</button>
+      <button onClick={sendRequest} type="button" className="w-96 mt-6 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 ">{type === "signup" ? "Sign Up" : "Sign In"}</button>
     </div>
   );
 };
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -5,12 +5,12 @@ import Auth from '../components/Auth'
 const Signup = () => {
   return (
     <div>
-      <div className='grid grid-cols-2'>
+      <div className='grid grid-cols-1 lg:grid-cols-2'>
         <div>
           <Auth type={'signup'} />
         </div >
 
-        <div className='invisible md:visible'>
+        <div className='hidden lg:block'>
           <Quote/>
         </div>
 
