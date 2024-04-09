32755d21ec5bf35f26a010609cd2d07d15377ce5
Sahil Meena <sahilmeena2031@gmail.com>
2024-04-09 12:03:44 +0530

    fixed the Signin/Signup Validators error

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,7 +1,5 @@
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import { SignupInput } from "@sahilxdev/medium-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
